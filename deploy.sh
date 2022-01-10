6bc7e5cc819a4140bd5110b18bf1a62af39c9892
Jonatan David Orozco Cantillo <jonorozcoc@gmail.com>
2022-01-10 18:55:12 -0500

    ci: removed unkown flag

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 VERSION="v$(grep 'version:' pubspec.yaml | sed -E 's/version: (\b[0-9].\b[0-9].\b[0-9])/\1/')"
-gh release create $VERSION --title "Release $VERSION" --generate-notes --draft ./versioning.exe
+gh release create $VERSION --title "Release $VERSION" --notes "New release $VERSION" --draft ./versioning.exe
