cae116b90d5066d11bd2cfa568f4a288ba802741
Jonatan David Orozco Cantillo <jonorozcoc@gmail.com>
2022-01-10 19:36:09 -0500

    ci: fixed issues with path

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 VERSION="v$(grep 'version:' pubspec.yaml | sed -E 's/version: (\b[0-9].\b[0-9].\b[0-9])/\1/')"
-gh release create $VERSION --title "Release $VERSION" --notes "New release $VERSION" --draft build/versioning-$VERSION-win64.exe
+gh release create $VERSION --draft --title "Release $VERSION" --notes "New release $VERSION" "$PWD/build/versioning-$VERSION-win64.exe"
