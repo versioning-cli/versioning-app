c4af22afc7a55545b73f8ef42b4fe34a7394cf7c
Jonatan David Orozco Cantillo <jonorozcoc@gmail.com>
2022-01-10 19:03:23 -0500

    ci: moved binaries to build

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 VERSION=$(grep 'version:' pubspec.yaml | sed -e 's?version: ??')
 sed -i -e "s/APP_VERSION/$VERSION/" ./lib/commands/version.dart
+if [[ ! -d ./build ]]; then
+    mkdir build
+fi
 if [[ "$OSTYPE" == "msys" ]]; then
-    dart compile exe bin/app.dart --output versioning.exe
+    dart compile exe bin/app.dart --output build/versioning-$VERSION-win64.exe
     sed -i -e "s/$VERSION/APP_VERSION/" ./lib/commands/version.dart
 fi
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 VERSION="v$(grep 'version:' pubspec.yaml | sed -E 's/version: (\b[0-9].\b[0-9].\b[0-9])/\1/')"
-gh release create $VERSION --title "Release $VERSION" --notes "New release $VERSION" --draft ./versioning.exe
+gh release create $VERSION --title "Release $VERSION" --notes "New release $VERSION" --draft ./build/versioning-$VERSION-win64.exe
+rm -r build
